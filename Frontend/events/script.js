1d2aa8eebb2a112318c8bfa8084a41c28636c1fd
Aman Yadav <amanyadav923949@gmail.com>
2025-02-07 10:45:23 +0530

    events page

--- Frontend/events/script.js
+++ Frontend/events/script.js
@@ -8,3 +8,12 @@ bar.addEventListener("click", () => {
     nav.style.maxHeight = "300px";
   }
 });
+
+const images = document.querySelectorAll(".carousel-image");
+let currentIndex = 0;
+
+setInterval(() => {
+  images[currentIndex].classList.remove("active");
+  currentIndex = (currentIndex + 1) % images.length;
+  images[currentIndex].classList.add("active");
+}, 3000);
